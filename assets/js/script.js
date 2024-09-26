618e1e48354652b064a8d895097485499ca58baa
Gaurav Jain <139974825+Gaurav9128@users.noreply.github.com>
2024-09-26 19:18:01 +0530

    Complete Source code

---
+++ assets/js/script.js
@@ -0,0 +1,170 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function(currentSlider) {
+
+  const sldierContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sldierContainer.style.transform = `translateX(-${sldierContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= sldierContainer.childElementCount - 1;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+
+   const slidePrev = function () {
+
+    if (currentSlidePos <= 0) {
+      currentSlidePos = sldierContainer.childElementCount - 1;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = sldierContainer.childElementCount <= 1;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = "none";
+    sliderPrevBtn.style.display = "none";
+  }
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
+
+
+
+/**
+ * ACCORDION
+ */
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  }
+
+  accordionBtn.addEventListener("click", expandAccordion);
+
+}
+
+for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function (e) {
+    e.preventDefault(); // Prevent default anchor behavior
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
